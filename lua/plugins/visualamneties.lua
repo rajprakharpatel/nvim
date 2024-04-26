c8b0f4a9f7b56362b049cb6cc116f57e5face1d6
rajprakhar <rajprakhar2000@gmail.com>
2024-04-26 16:00:30 +0530

    remove specs.nvim

--- lua/plugins/visualamneties.lua
+++ lua/plugins/visualamneties.lua
@@ -26,27 +26,27 @@ return {
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 		config = true,
 	},
-	{
-		"edluffy/specs.nvim",
-		event = "VeryLazy",
-		config = function()
-			require("specs").setup {
-				show_jumps = true,
-				min_jump = 5,
-				popup = {
-					delay_ms = 0, -- delay before popup displays
-					inc_ms = 4, -- time increments used for fade/resize effects
-					blend = 20, -- starting blend, between 0-100 (fully transparent), see :h winblend
-					width = 50,
-					winhl = "PMenu",
-					fader = require("specs").linear_fader,
-					resizer = require("specs").shrink_resizer,
-				},
-				ignore_filetypes = { "dashboard" },
-				ignore_buftypes = { nofile = true },
-			}
-		end,
-	},
+-- 	{
+-- 		"edluffy/specs.nvim",
+-- 		event = "VeryLazy",
+-- 		config = function()
+-- 			require("specs").setup {
+-- 				show_jumps = true,
+-- 				min_jump = 5,
+-- 				popup = {
+-- 					delay_ms = 0, -- delay before popup displays
+-- 					inc_ms = 4, -- time increments used for fade/resize effects
+-- 					blend = 20, -- starting blend, between 0-100 (fully transparent), see :h winblend
+-- 					width = 50,
+-- 					winhl = "PMenu",
+-- 					fader = require("specs").linear_fader,
+-- 					resizer = require("specs").shrink_resizer,
+-- 				},
+-- 				ignore_filetypes = { "dashboard" },
+-- 				ignore_buftypes = { nofile = true },
+-- 			}
+-- 		end,
+-- 	},
 	{
 		"rcarriga/nvim-notify",
 		lazy = false,
